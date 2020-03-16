edf15086a4d50ceeb762c2c3af7be290dec952fe
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-03-16 15:02:09 -0400

    Add reducer and render items

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
+import { initialState, reducer } from "./reducers/reducer";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <header className="App-header">
        Todo Project
       </header>
+      <div className = "tasks">
+        {state.map(item => 
+        <p>{item.item}</p>
+        )}
+        </div>
+        <input></input>
+        <button>Add Task</button>
+        <br/>
+        <button>Clear Completed</button>
     </div>
   );
 }
---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,21 @@
+export const initialState = [
+{
+    item: "Walk the dog",
+    completed: false,
+    id: 74382374837
+},
+{
+    item: "Buy milk",
+    completed: false,
+    id: 8439048329
+},
+{
+    item: "Read the paper",
+    completed: false,
+    id: 8439048329
+}
+];
+
+export const reducer = (state, action) => {
+    return state;
+}
